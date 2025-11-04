c12f6f8a03da9c07647f9462cc7e66b3aa53d04c
rameshrajanr6 <129479888+rameshrajanr6@users.noreply.github.com>
2025-11-03 21:11:14 -0500

    Update provider.tf

--- vpc_module2/provider.tf
+++ vpc_module2/provider.tf
@@ -2,3 +2,11 @@ provider "aws" {
     region = var.vpc-region
     
 }
+
+terraform {
+ backend "s3" {
+   bucket = "prod-bucket-95e8"
+    key    = "prodvpcconfig/prodvpckey"
+    region = "us-east-1"
+  }
+}
